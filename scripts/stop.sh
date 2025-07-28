a490d88b43724896c9ac8211c54ddff1d7c628b6
J-millar99 <wlwogus33@gmail.com>
2025-07-28 05:09:29 +0000

    docker compose 파일 수정과 stop 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,25 +1,8 @@
-#!/bin/bash
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 
-# Kafka 클러스터 중지 스크립트
-echo "Kafka 클러스터를 중지합니다..."
+docker rmi -f $(docker images -aq)
 
-# 역순으로 중지 (의존성을 고려)
-echo "1. Control Center 중지..."
-docker-compose -f ../inventory/docker-compose-c3.yml down
+docker volume rm $(docker volume ls -q)
 
-echo "2. ksqlDB 중지..."
-docker-compose -f ../inventory/docker-compose-db.yml down
-
-echo "3. Kafka Connect 중지..."
-docker-compose -f ../inventory/docker-compose-cw.yml down
-
-echo "4. Schema Registry 중지..."
-docker-compose -f ../inventory/docker-compose-sr.yml down
-
-echo "5. Broker 노드들 중지..."
-docker-compose -f ../inventory/docker-compose-br.yml down
-
-echo "6. Controller 노드들 중지..."
-docker-compose -f ../inventory/docker-compose-kr.yml down
-
-echo "모든 서비스가 중지되었습니다!"
+docker network rm $(docker network ls | grep -v "bridge\|host\|none" | awk '{print $1}')
